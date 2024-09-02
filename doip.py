18122eef9dc65a68f190b5afa2c4aae1523fd478
Aleksey Kirilishin <alexkak@inbox.ru>
2024-09-02 17:17:44 +0400

    rev_0.2

--- doip.py
+++ doip.py
@@ -1,18 +1,14 @@
 import os
 import requests
 
-# Указываем URL, откуда будем загружать файл с IP-адресами
 url = 'https://raw.githubusercontent.com/Alvel007/swinip/main/ips.txt'
 
-# Локальный путь к конфигурационному файлу network
-config_path = 'network'
+config_path = '/etc/config/network'
 
 wg_name = 'sit9'
 
-# Интерфейс WireGuard, который нужно обновить
 wg_interface_name = 'amneziawg_sit9'
 
-# Функция для загрузки файла с GitHub
 def download_ip_list(url):
     try:
         response = requests.get(url)
@@ -26,10 +22,10 @@ def download_ip_list(url):
                     seen.add(line)
             return ip_list
         else:
-            print("Не удалось загрузить файл с IP-адресами")
+            print("Error download")
             return []
     except requests.RequestException as e:
-        print(f"Произошла ошибка при загрузке файла: {e}")
+        print(f"Error download: {e}")
         return []
 
 
@@ -43,29 +39,25 @@ def update_wireguard_config(ip_list):
     found_endpoint_port = False
 
     for line in config_lines:
-        # Проверяем, нашли ли мы нужный интерфейс
         if line.strip().startswith(f"config {wg_interface_name}"):
             found_interface = True
-            added_ips = False  # Обнуляем флаг добавления IP для нового интерфейса
-            found_endpoint_port = False  # Обнуляем флаг, когда нашли новый интерфейс
+            added_ips = False
+            found_endpoint_port = False
             new_config_lines.append(line)
             continue
         
-        # Если мы находимся внутри нужного интерфейса
         if found_interface:
             if line.strip().startswith("list allowed_ips"):
-                # Пропускаем все старые записи list allowed_ips
                 continue
             elif line.strip().startswith("option endpoint_port"):
-                found_endpoint_port = True  # Помечаем, что нашли нужную строку
+                found_endpoint_port = True
                 new_config_lines.append(line)
                 continue
             elif found_endpoint_port and not added_ips:
-                # Добавляем новые IP-адреса после строки с option endpoint_port
                 for ip in ip_list:
                     new_config_lines.append(f"\tlist allowed_ips '{ip}/32'\n")
-                added_ips = True  # Устанавливаем флаг, что IP-адреса добавлены
-                found_interface = False  # Завершаем обработку текущего интерфейса
+                added_ips = True
+                found_interface = False
 
             new_config_lines.append(line)
         else:
@@ -74,11 +66,10 @@ def update_wireguard_config(ip_list):
     with open(config_path, "w", encoding="utf-8") as file:
         file.writelines(new_config_lines)
 
-    print(f"Обновление конфигурации завершено. Добавлено {len(ip_list)} IP-адресов.")
+    print(f"successful {len(ip_list)} IP.")
 
 update_wireguard_config(download_ip_list(url))
 
-# Функция для перезапуска интерфейса WireGuard
 def restart_wireguard_interface():
     os.system(f"ifdown {wg_name}")
     os.system(f"ifup {wg_name}")
@@ -89,6 +80,6 @@ if __name__ == "__main__":
     if ip_list:
         update_wireguard_config(ip_list)
         restart_wireguard_interface()
-        print("Конфигурация WireGuard успешно обновлена и интерфейс перезапущен.")
+        print("Everything was successful.")
     else:
-        print("Список IP пуст или не удалось его загрузить.")
+        print("It doesn't work.")
